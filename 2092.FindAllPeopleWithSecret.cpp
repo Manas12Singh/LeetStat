f7020224f1a1e4089e71a53a9faab77d8e702960
Manas12Singh <89171896+Manas12Singh@users.noreply.github.com>
2024-02-24 22:44:48 +0530

    Update 2092.FindAllPeopleWithSecret.cpp

--- 2092.FindAllPeopleWithSecret.cpp
+++ 2092.FindAllPeopleWithSecret.cpp
@@ -3,12 +3,10 @@ class Solution
 public:
     void helper(map<int, int> &s, map<int, map<int, set<int>>> &m, int n, int t)
     {
-        if (s.find(n) == s.end())
+        if (s.find(n) == s.end() || s[n]>t)
             s[n] = t;
-        else if (s[n] <= t)
-            return;
         else
-            s[n] = t;
+            return;
         for (auto &i : m[n])
         {
             if (i.first >= s[n])
@@ -19,11 +17,11 @@ public:
     vector<int> findAllPeople(int n, vector<vector<int>> &meetings, int firstPerson)
     {
         map<int, map<int, set<int>>> m;
+        m[0][0].insert(firstPerson);
         for (auto &i : meetings)
             m[i[0]][i[2]].insert(i[1]), m[i[1]][i[2]].insert(i[0]);
         map<int, int> s;
         helper(s, m, 0, 0);
-        helper(s, m, firstPerson, 0);
         vector<int> res;
         for (auto &i : s)
             res.push_back(i.first);
