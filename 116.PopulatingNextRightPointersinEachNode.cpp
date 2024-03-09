6abb3255fff22a5e9489564605a8cb8c7086d1b3
Manas12Singh <89171896+Manas12Singh@users.noreply.github.com>
2024-03-09 08:12:00 +0530

    Update 116.PopulatingNextRightPointersinEachNode.cpp

--- 116.PopulatingNextRightPointersinEachNode.cpp
+++ 116.PopulatingNextRightPointersinEachNode.cpp
@@ -1,1 +1,17 @@
-https://leetcode.com/problems/populating-next-right-pointers-in-each-node/submissions/1198095635
+class Solution
+{
+public:
+    void helper(Node *root, Node *next)
+    {
+        if (root == nullptr)
+            return;
+        root->next = next;
+        helper(root->left, root->right);
+        helper(root->right, (next) ? next->left : next);
+    }
+    Node *connect(Node *root)
+    {
+        helper(root, nullptr);
+        return root;
+    }
+};
